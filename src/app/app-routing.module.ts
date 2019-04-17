c14513f9264ac5a72ebaa8d21ea2695d413f8d26
KorNatalya <natalie178@ya.ru>
2019-04-17 14:00:29 +0300

    home-screen was created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from './system/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'system', pathMatch: 'full'},
+  {path: 'system', loadChildren: './system/system.module#SystemModule'},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/system/system.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SystemRoutingModule} from './system-routing.module';
+import {SystemComponent} from './system.component';
+import { HomeScreenComponent } from './main-landing/home-screen/home-screen.component';
+import { AboutUsComponent } from './main-landing/about-us/about-us.component';
+import { SkillsComponent } from './main-landing/skills/skills.component';
+import { PortfolioComponent } from './main-landing/portfolio/portfolio.component';
+import { FooterComponent } from './main-landing/footer/footer.component';
+import {MatButtonModule, MatListModule} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SystemRoutingModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  declarations: [
+    SystemComponent,
+    HomeScreenComponent,
+    AboutUsComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    FooterComponent
+  ],
+  providers: []
+})
+
+export class SystemModule {
+
+}
