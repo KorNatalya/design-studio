306fe421244b2a951639a210a1c56e71c942dbfd
KorNatalya <natalie178@ya.ru>
2019-04-17 16:17:18 +0300

    skills and about-us blocks were created

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -7,14 +7,15 @@ import { AboutUsComponent } from './main-landing/about-us/about-us.component';
 import { SkillsComponent } from './main-landing/skills/skills.component';
 import { PortfolioComponent } from './main-landing/portfolio/portfolio.component';
 import { FooterComponent } from './main-landing/footer/footer.component';
-import {MatButtonModule, MatListModule} from '@angular/material';
+import {MatButtonModule, MatListModule, MatProgressBarModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     SystemRoutingModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ],
   declarations: [
     SystemComponent,
